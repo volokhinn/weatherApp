5b5be1287929b34b4446cc48b942304326df4932
volokhinn <csgomx@mail.ru>
2023-07-27 11:18:34 +0700

    HomeScreen is done

---
+++ api/weather.js
@@ -0,0 +1,28 @@
+import axios from "axios";
+
+import { apiKey } from "../constants";
+
+const forecastEndPoint = params => `https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${params.cityName}&days=${params.days}&aqi=no&alerts=no`
+const locationsEndPoint = params => `https://api.weatherapi.com/v1/search.json?key=${apiKey}&q=${params.cityName}`
+
+const apiCall = async (endpoint) => {
+    const options = {
+        method: 'GET',
+        url: endpoint
+    }
+    try {
+        const response = await axios.request(options);
+        return response.data;
+    }
+    catch (err) {
+        console.log(err)
+        return null;
+    }
+}
+
+export const fetchWeatherForecast = params => {
+    return apiCall(forecastEndPoint(params));
+}
+export const fetchLocations = params => {
+    return apiCall(locationsEndPoint(params));
+} 
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -3,8 +3,9 @@ import React, { useState } from 'react';
 import { View, Text, TextInput, TouchableOpacity } from 'react-native';
 import { Image } from 'react-native';
 import { SafeAreaView } from 'react-native';
-import { MagnifyingGlassIcon } from 'react-native-heroicons/outline';
+import { CalendarDaysIcon, MagnifyingGlassIcon } from 'react-native-heroicons/outline';
 import { MapPinIcon } from 'react-native-heroicons/solid';
+import { ScrollView } from 'react-native';
 
 const HomeScreen = () => {
   const [showSearch, setShowSearch] = useState(false);
@@ -24,6 +25,7 @@ const HomeScreen = () => {
         <View className="flex-row justify-end items-center rounded-full relative">
           {showSearch ? (
             <TextInput
+              onChangeText={handleSearch}
               placeholder="Выберите город"
               placeholderTextColor={'white'}
               style={{
@@ -53,6 +55,50 @@ const HomeScreen = () => {
             </View>
           ) : null}
         </View>
+      </View>
+      {/* Прогноз */}
+      <View className="mx-4 flex justify-around flex-1 mb-2">
+        <Text className="text-white text-center text-2xl font-bold mr-2">
+          Новосибирск,
+          <Text className="text-lg font-semibold text-gray-300">Россия</Text>
+        </Text>
+        {/* Иконка погоды */}
+        <View className="flex-row justify-center">
+          <Image source={require('../assets/images/partlycloudy.png')} className="w-52 h-52"></Image>
+        </View>
+        <View className="space-y-4">
+          <Text className="text-white text-center text-4xl">23&#176;</Text>
+          <Text className="text-white text-center text-xl tracking-widest">Малооблачно</Text>
+        </View>
+      </View>
+      {/* Другая статистика */}
+      <View className="flex-row justify-between mx-4 my-4">
+        <View className="flex-row space-x-2 items-center">
+          <Image source={require('../assets/icons/wind.png')} className="h-6 w-6"></Image>
+          <Text className="text-white font-semibold text-base">4 m/s</Text>
+        </View>
+        <View className="flex-row space-x-2 items-center">
+          <Image source={require('../assets/icons/drop.png')} className="h-6 w-6"></Image>
+          <Text className="text-white font-semibold text-base">23%</Text>
+        </View>
+        <View className="flex-row space-x-2 items-center">
+          <Image source={require('../assets/icons/sunny.png')} className="h-6 w-6"></Image>
+          <Text className="text-white font-semibold text-base">6:05 AM</Text>
+        </View>
+      </View>
+      {/* Прогноз на другие дни */}
+      <View className="mb-2 space-y-3">
+        <View className="flex-row items-center mx-5 space-x-2">
+          <CalendarDaysIcon size="20" color="white" />
+          <Text className="text-white text-base">Прогноз по дням</Text>
+        </View>
+        <ScrollView horizontal currentContainerStyle={{paddingHorizontal: 15}} showsHorizontalScrollIndicator={false} className="mx-1">
+          <View style={{backgroundColor: "rgba(165,165,165,0.5)"}} className="flex justify-center items-center w-20 rounded-3xl py-3 space-y-1 mr-4 bg-opacity-20">
+            <Image source={require('../assets/images/heavyrain.png')} className="w-10 h-10"></Image>
+              <Text className="text-white">Пн</Text>
+              <Text className="text-white text-xl font-semibold">13&#176;</Text>
+          </View>
+        </ScrollView>
       </View>
     </View>
   );
